2e8029d4c58d857af02897e528bc5e7751e5697e
KanekiPrograming <54086856+KanekiPrograming@users.noreply.github.com>
2020-12-14 19:17:45 -0500

    td

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 const express = require("express");
 const convertidor = require("./app.js");
+const port = process.env.PORT || 3977;
 const bodyParser = require("body-parser");
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+// Configure Header HTTP
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+  res.header("Allow", "GET, POST, OPTIONS, PUT, DELETE");
+  next();
+});
+
+
 
 app.post("/getjs", async (request, response) => {
   if (request.body.piocode){
@@ -26,4 +41,8 @@ app.get("/", async (request, response) => {
   response.send("hola mundo");
 });
 
-app.listen("8010");
+app.listen(port , () => {
+  console.log("######################");
+  console.log("###### API REST ######");
+  console.log("######################");
+});
